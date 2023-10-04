6cebb1b72e796c37736f773a3d105b60849c76bf
viswan8 <aniruddhviswanadha2000@gmail.com>
2023-10-04 18:05:05 +0530

    Day1

--- Java/containsDuplicate.java
+++ Java/containsDuplicate.java
@@ -1,3 +1,13 @@
+/* 
+LeetCode 217: Contains Duplicates
+https://leetcode.com/problems/contains-duplicate/
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Note: While copying the code please write your own main method.
+*/
+
 class containsDuplicate {
     public boolean containsDuplicate(int[] nums) {
         HashSet <Integer> Unique = new HashSet<>();
