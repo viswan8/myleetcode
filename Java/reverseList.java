4b1b0e4bd886f9fe849a8c8f4cadea42e9cad9f3
viswan8 <aniruddhviswanadha2000@gmail.com>
2023-10-06 23:29:46 +0530

    Day3

--- Java/reverseList.java
+++ Java/reverseList.java
@@ -1,3 +1,14 @@
+/* 
+LeetCode 206: Reverse of LinkedList
+https://leetcode.com/problems/reverse-linked-list
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Note: While copying the code please write your own main method.
+*/
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,6 +19,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+//Iterative Method
 class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode currNode = head;
