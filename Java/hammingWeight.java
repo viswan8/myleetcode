afbba973cf7aba32e530acae133ea6f5ee9c42f1
viswan8 <aniruddhviswanadha2000@gmail.com>
2023-10-16 23:33:42 +0530

    Day11 smart change

--- Java/hammingWeight.java
+++ Java/hammingWeight.java
@@ -24,3 +24,17 @@ public class Solution {
         return count;
     }
 }
+
+//smartway
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count =0;
+        while(n!=0)
+        {
+            n = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+}
