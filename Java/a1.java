f64e179a7429a7fb49cc7e2c50676198fda83537
viswan8 <aniruddhviswanadha2000@gmail.com>
2023-10-18 23:08:43 +0530

    Day13

---
+++ Java/a1.java
@@ -0,0 +1,42 @@
+/* 
+LeetCode 202. Happy Number
+
+https://leetcode.com/problems/happy-number/
+
+Time Complexity: O(n) since 3 individual loops running till n
+Space Complexity: O(n) since 2 arrays of size n are created
+
+Note: While copying the code please write your own main method.
+*/
+
+class Solution {
+    public boolean isHappy(int n) {
+        if(n==1 || n==-1)
+            return true;
+        HashSet<Integer> visit = new HashSet<>();
+
+        while(!visit.contains(n))
+        {
+            visit.add(n);
+            n = sumOfSquare(n);
+
+            if(n==1)
+                return true;
+        }
+
+        return false;
+    }
+
+    public int sumOfSquare(int n){
+        int output =0;
+        while(n !=0)
+        {
+            int temp = n%10;
+            temp = temp * temp;
+            output += temp;
+            n /= 10;
+        }
+        return output;
+    }
+
+}
